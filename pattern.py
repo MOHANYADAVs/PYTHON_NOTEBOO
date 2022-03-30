a3918ef3a2dfbb10aa567c02dad5cc20e98511a0
Mohan Yadav <92673465+MOHANYADAV555@users.noreply.github.com>
2022-03-30 07:32:35 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,12 @@
+num = int(input("enter number of rows : "))
+
+for i in range (0,num):
+
+    for j in range (0,num-i-1):
+
+        print(end= " ")
+
+    for j in range (0,j+1):
+
+        print("*",end=" ")
+    print(j)
